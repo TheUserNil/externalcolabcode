19cb24a36a503bccc9bfe2505a6a2d2fb7f784b3
blaise <133521603+blaise-tk@users.noreply.github.com>
2023-08-11 02:58:52 +0200

    Update clone_alt.py

--- clone_alt.py
+++ clone_alt.py
@@ -153,6 +153,7 @@ def run_script():
         run_cmd("git checkout 2e78c126a43de0b9a43e57b46f0dfc628cf87933")
         run_cmd("git checkout 208995769fd22bce0a0e62a3407169bf6d776eae")
         run_cmd("git checkout 739463f58f15fd143c102f58458d483d719f6cf5")
+        run_cmd("git checkout d555bcd2541ce3e78a9391f9255eb17f9753b006")
 
         run_cmd("wget https://github.com/kalomaze/Mangio-Kalo-Tweaks/raw/patch-1/EasierGUI.py")
 
