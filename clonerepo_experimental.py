3636638910986eb0a80f5662eb5190b2f07874e3
TheUserNil <102625768+TheUserNil@users.noreply.github.com>
2023-09-06 16:18:05 -0500

    Update clonerepo_experimental.py

--- clonerepo_experimental.py
+++ clonerepo_experimental.py
@@ -165,7 +165,7 @@ def run_script():
 
     # Download the torchcrepe folder from the maxrmorrison/torchcrepe repository
     run_cmd("git clone https://github.com/maxrmorrison/torchcrepe.git")
-    shutil.move('/content/torchcrepe/torchcrepe', xdsame')
+    shutil.move('/content/torchcrepe/torchcrepe', xdsame)
     shutil.rmtree('/content/torchcrepe', ignore_errors=True)  # Delete the torchcrepe repository folder
 
     # Change the current directory to /content/Retrieval-based-Voice-Conversion-WebUI
