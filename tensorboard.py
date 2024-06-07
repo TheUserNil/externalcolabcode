29f19342d4cea6e1abe90f36f56460aea53c9791
Vidalnt <102625768+Vidalnt@users.noreply.github.com>
2024-06-06 19:49:53 -0500

    Create tensorboard.py

---
+++ tensorboard.py
@@ -0,0 +1,19 @@
+import gradio as gr
+
+def tensorboard():
+  with gr.Row():
+    gr.Markdown("## Tensorboard")
+    with gr.Row():
+      gr.HTML(
+                """
+                <iframe width="100%" 
+                    height="700" 
+                    src="http://localhost:6006" 
+                    title="Tensordboard" 
+                    frameborder="0" 
+                    allowfullscreen>
+                </iframe>
+                """,
+                label="Tensorboard",
+                show_label=True
+            ) 
